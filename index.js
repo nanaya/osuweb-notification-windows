81f9eee66e8ea88b7037d2bfd937c439ca33ebb2
nanaya <me@nanaya.net>
2023-01-28 05:18:41 +0900

    Initial

---
+++ index.js
@@ -0,0 +1,29 @@
+import Notifier from 'node-notifier';
+import Net from 'net';
+
+function generate (result) {
+  let message;
+  let icon;
+
+  if (result === 'fail') {
+    message = 'Compilation failed';
+    icon = 'fail.png';
+  } else {
+    message = 'Compilation succeeded';
+    icon = 'ok.png';
+  }
+
+  return {
+    title: 'Webpack',
+    message,
+    icon
+  };
+}
+
+const server = Net.createServer(function (socket) {
+  socket.on('data', function (result) {
+    Notifier.notify(generate(result.toString()));
+  });
+});
+
+server.listen(4900, '10.0.2.2');
